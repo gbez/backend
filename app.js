2bebae07d557b86f403646d3e0234b1c818b245b
Grant Besner <gmbmonkeyvp@gmail.com>
2021-06-23 15:58:46 -0400

    cors added

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ const app = express();
 
 // Add Middleware
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: true }));
